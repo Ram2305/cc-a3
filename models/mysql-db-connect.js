7d4c7c08946cd673250e548705697366038ef7ac
Ram2305 <vadapalli.j@northeastern.edu>
2024-02-05 04:44:57 -0500

    new code 5

--- models/mysql-db-connect.js
+++ models/mysql-db-connect.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize({
   dialect: 'mysql',
-  host: process.env.DB_HOST,
+  host: 'localhost',
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
